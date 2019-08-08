e9ab272ba9c6ec141122b061c9def99944460f16
Miguel Zetina <loefthur26@gmail.com>
2019-08-08 17:16:33 -0500

    Add more registers 10k

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -2,8 +2,10 @@ import csvdownload.*
 
 class BootStrap {
     def init = { servletContext ->
-        new Book(title: 'Groovy for Domain-Specific Languages', author: 'Fergal Dearle').save()
-        new Book(title: 'Programming Groovy 2: Dynamic Productivity for the Java Developer', author: 'Venkat Subramaniam').save()
+        (0..10000).each {
+            new Book(title: 'Groovy for Domain-Specific Languages', author: 'Fergal Dearle').save()
+            new Book(title: 'Programming Groovy 2: Dynamic Productivity for the Java Developer', author: 'Venkat Subramaniam').save()
+        }
     }
     def destroy = {
     }
